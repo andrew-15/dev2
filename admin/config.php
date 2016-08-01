67fad53e9f1b32517d7d0102a4461bebf3d4cfe2
andrew-15 <andrey_syrota@mail.ru>
2016-08-01 08:53:11 +0300

    dev5
    dev5

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dev5.dreamvention.com/admin/');
+define('HTTP_CATALOG', 'http://dev5.dreamvention.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dev5.dreamvention.com/admin/');
+define('HTTPS_CATALOG', 'http://dev5.dreamvention.com/');
+
+// DIR
+define('DIR_APPLICATION', 'F:/OpenServer/domains/dev5.dreamvention.com/admin/');
+define('DIR_SYSTEM', 'F:/OpenServer/domains/dev5.dreamvention.com/system/');
+define('DIR_LANGUAGE', 'F:/OpenServer/domains/dev5.dreamvention.com/admin/language/');
+define('DIR_TEMPLATE', 'F:/OpenServer/domains/dev5.dreamvention.com/admin/view/template/');
+define('DIR_CONFIG', 'F:/OpenServer/domains/dev5.dreamvention.com/system/config/');
+define('DIR_IMAGE', 'F:/OpenServer/domains/dev5.dreamvention.com/image/');
+define('DIR_CACHE', 'F:/OpenServer/domains/dev5.dreamvention.com/system/storage/cache/');
+define('DIR_DOWNLOAD', 'F:/OpenServer/domains/dev5.dreamvention.com/system/storage/download/');
+define('DIR_LOGS', 'F:/OpenServer/domains/dev5.dreamvention.com/system/storage/logs/');
+define('DIR_MODIFICATION', 'F:/OpenServer/domains/dev5.dreamvention.com/system/storage/modification/');
+define('DIR_UPLOAD', 'F:/OpenServer/domains/dev5.dreamvention.com/system/storage/upload/');
+define('DIR_CATALOG', 'F:/OpenServer/domains/dev5.dreamvention.com/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'developer_5');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dev5.dreamvention.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dev5.dreamvention.com/');
+
+// DIR
+define('DIR_APPLICATION', 'F:/OpenServer/domains/dev5.dreamvention.com/catalog/');
+define('DIR_SYSTEM', 'F:/OpenServer/domains/dev5.dreamvention.com/system/');
+define('DIR_LANGUAGE', 'F:/OpenServer/domains/dev5.dreamvention.com/catalog/language/');
+define('DIR_TEMPLATE', 'F:/OpenServer/domains/dev5.dreamvention.com/catalog/view/theme/');
+define('DIR_CONFIG', 'F:/OpenServer/domains/dev5.dreamvention.com/system/config/');
+define('DIR_IMAGE', 'F:/OpenServer/domains/dev5.dreamvention.com/image/');
+define('DIR_CACHE', 'F:/OpenServer/domains/dev5.dreamvention.com/system/storage/cache/');
+define('DIR_DOWNLOAD', 'F:/OpenServer/domains/dev5.dreamvention.com/system/storage/download/');
+define('DIR_LOGS', 'F:/OpenServer/domains/dev5.dreamvention.com/system/storage/logs/');
+define('DIR_MODIFICATION', 'F:/OpenServer/domains/dev5.dreamvention.com/system/storage/modification/');
+define('DIR_UPLOAD', 'F:/OpenServer/domains/dev5.dreamvention.com/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'developer_5');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
